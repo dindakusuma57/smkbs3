a471fc2cac17f1c11ca698fd9f66618d4655d9d3
dindakusuma57 <dindakusuma@apps.ipb.ac.id>
2024-11-06 13:34:58 +0700

    completed fillament at profile

---
+++ app/Filament/Resources/MisiResource.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MisiResource\Pages;
+use App\Filament\Resources\MisiResource\RelationManagers;
+use App\Models\Misi;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Tables\Columns\ImageColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MisiResource extends Resource
+{
+    protected static ?string $model = Misi::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Profile';
+
+    protected static ?int $navigationSort = 3;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            Section::make([
+                Grid::make()
+                    ->schema([
+                        TextInput::make('judul')
+                            ->required()
+                            ->maxLength(255),
+
+                        MarkdownEditor::make('deskripsi')
+                            ->columnSpanFull()
+                            ->fileAttachmentsDirectory('misi'),
+
+                        FileUpload::make('image')
+                            ->image()
+                            ->directory('misi')
+                    ])
+            ])
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns([
+            Tables\Columns\TextColumn::make('judul')
+                ->searchable(),
+            Tables\Columns\TextColumn::make('deskripsi')
+                ->searchable(),
+            ImageColumn::make('image')
+                ->label('Image')
+                ->width(100)
+                ->height(100),
+            Tables\Columns\TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('updated_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMisis::route('/'),
+            'create' => Pages\CreateMisi::route('/create'),
+            'edit' => Pages\EditMisi::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/VisiResource.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VisiResource\Pages;
+use App\Filament\Resources\VisiResource\RelationManagers;
+use App\Models\Visi;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Tables\Columns\ImageColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class VisiResource extends Resource
+{
+    protected static ?string $model = Visi::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Profile';
+
+    protected static ?int $navigationSort = 2;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            Section::make([
+                Grid::make()
+                    ->schema([
+                        TextInput::make('judul')
+                            ->required()
+                            ->maxLength(255),
+
+                        MarkdownEditor::make('deskripsi')
+                            ->columnSpanFull()
+                            ->fileAttachmentsDirectory('visi'),
+
+                        FileUpload::make('image')
+                            ->image()
+                            ->directory('visi')
+                    ])
+            ])
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns([
+            Tables\Columns\TextColumn::make('judul')
+                ->searchable(),
+            Tables\Columns\TextColumn::make('deskripsi')
+                ->searchable(),
+            ImageColumn::make('image')
+                ->label('Image')
+                ->width(100)
+                ->height(100),
+            Tables\Columns\TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('updated_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVisis::route('/'),
+            'create' => Pages\CreateVisi::route('/create'),
+            'edit' => Pages\EditVisi::route('/{record}/edit'),
+        ];
+    }
+}
